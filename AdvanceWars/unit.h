51bfeaf04d991b822dc2b7efbebd3985326690b6
ulb-tkubla <tkubla@ulb.ac.be>
2018-11-10 18:27:29 +0100

    Fix indents

--- AdvanceWars/unit.h
+++ AdvanceWars/unit.h
@@ -22,43 +22,43 @@ class Unit : public GameObject{
 	int health;
 	int price;
 
-  std::vector<Edge> ee[XDIM * YDIM]; //static + still hardcoded
+    std::vector<Edge> ee[XDIM * YDIM]; //static + still hardcoded
 
 public:
-  Unit(int x=0, int y=0, int type =0, char team='\0');
-  //int getPosX();
-  //int getPosY();
-  void attack(Unit & un, bool counter=false);
-  int getHealth() const;
-  void heal(int qnt);
-  int getPrice() const;
-  int getUnitType() const;
-  char getTeam() const;
-  void sufferDamage(int & dmg);
-  void die();
-  virtual bool move(int dir, int terrainMod);
-  //bool operator== (Unit& un) const;
-  //bool operator== (GameObject& obj) const;
-  void endTurn();
-  void newTurn();
+    Unit(int x=0, int y=0, int type =0, char team='\0');
+    //int getPosX();
+    //int getPosY();
+    void attack(Unit & un, bool counter=false);
+    int getHealth() const;
+    void heal(int qnt);
+    int getPrice() const;
+    int getUnitType() const;
+    char getTeam() const;
+    void sufferDamage(int & dmg);
+    void die();
+    virtual bool move(int dir, int terrainMod);
+    //bool operator== (Unit& un) const;
+    //bool operator== (GameObject& obj) const;
+    void endTurn();
+    void newTurn();
 
-  std::vector<std::vector<int> > selected();
-  virtual std::string getType() const;
-  void interactWith();
+    std::vector<std::vector<int> > selected();
+    virtual std::string getType() const;
+    void interactWith();
 private:
-  int getPosFromCoord(int x, int y);
-  int* getCoordFromPos(int pos, int (&cr)[2]);
-  std::vector<Edge> findConnected(int pos);
-  std::vector<int> bfs();
-  std::vector<int> dijkstra();
+    int getPosFromCoord(int x, int y);
+    int* getCoordFromPos(int pos, int (&cr)[2]);
+    std::vector<Edge> findConnected(int pos);
+    std::vector<int> bfs();
+    std::vector<int> dijkstra();
 
-  // dmg_chart[attacker][target]
-  // 0 = infantry, 1 = bazooka, 2 = recon, 3 = anti-air
-  // 4 = tank, 5 = md. tank, 6 = mega tank, 7 = neotank
-  // 8 = b-copter, 9 = fighter, 10 = bomber
-  const static int dmg_chart[11][11];
-  const static int speed_chart[11];
-  const static int price_chart[11];
+    // dmg_chart[attacker][target]
+    // 0 = infantry, 1 = bazooka, 2 = recon, 3 = anti-air
+    // 4 = tank, 5 = md. tank, 6 = mega tank, 7 = neotank
+    // 8 = b-copter, 9 = fighter, 10 = bomber
+    const static int dmg_chart[11][11];
+    const static int speed_chart[11];
+    const static int price_chart[11];
 };
 
 #endif
