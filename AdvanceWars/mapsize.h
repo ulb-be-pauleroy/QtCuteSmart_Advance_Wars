e4cf19ad8c06e043690e1e3a2e6ce6ef564f9abd
pauleroy <polouloubis@yahoo.com>
2018-11-28 10:47:00 +0100

    Add BUGS.txt for feedback

--- AdvanceWars/mapsize.h
+++ AdvanceWars/mapsize.h
@@ -4,8 +4,8 @@
 #define XDIM 15 //map size from time it was a 2d array of vectors,
 #define YDIM 15 //to remove next and replace with size of actual map (3d vector)
 
-//extern int XDIM;
-//extern int YDIM;
+//extern int XDIM = 15;
+//extern int YDIM = 15;
 
 #endif // MAPSIZE_H
 
